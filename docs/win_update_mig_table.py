e0b328fbabe70d9a2b84e69533eb23c3009d7c63
Eshton Brogan <57577574+caesium24@users.noreply.github.com>
2021-12-15 14:20:29 -0500

    Create win_update_mig_table.py

---
+++ docs/win_update_mig_table.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2021, Eshton Brogan
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'private'}
+
+DOCUMENTATION = r'''
+---
+module: win_update_mig_table
+version_added: "2.9"
+short_description: Updates a Group Policy Migration table by comparing AD object names with those in the migration table
+description:
+     - Updates a Group Policy Migration table by comparing AD object names with those in the migration table and only works in environments with like names with only domain names or partial name differences.
+options:
+  path:
+    description:
+      - Path to GPO backup folder
+    type: str
+    required: yes
+  migration_table:
+    description:
+      - Path to Migration Table
+    type: str
+    required: yes
+notes:
+  - This must be run on a host that has the ActiveDirectory powershell module installed.
+ 
+author:
+    - Eshton Brogan
+'''
+
+EXAMPLES = r'''
+- name: Generate Migration Table
+  win_update_mig_table:
+    path: c:\backups
+    migration_table: 'c:\temp\example.migtable'    
+
+'''
+
+RETURN = r'''
+'''
